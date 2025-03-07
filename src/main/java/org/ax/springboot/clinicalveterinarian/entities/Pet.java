24de443151a6299e68303bb6dd19ba372a1f2733
AlexMartGong <155708055+AlexMartGong@users.noreply.github.com>
2025-03-07 13:58:28 -0600

    add constructors and getter/setter methods to Pet entity class

--- src/main/java/org/ax/springboot/clinicalveterinarian/entities/Pet.java
+++ src/main/java/org/ax/springboot/clinicalveterinarian/entities/Pet.java
@@ -56,4 +56,125 @@ public class Pet {
     @OneToMany(mappedBy = "pet")
     private List<Vaccination> vaccinations;
 
+    public Pet() {
+    }
+
+    public Pet(LocalDate birthDate, Breed breed, String color, List<Consultation> consultations, Gender gender, String microchip, String name, Owner owner, String photoUrl, Species species, List<Vaccination> vaccinations, BigDecimal weightKg) {
+        this.birthDate = birthDate;
+        this.breed = breed;
+        this.color = color;
+        this.consultations = consultations;
+        this.gender = gender;
+        this.microchip = microchip;
+        this.name = name;
+        this.owner = owner;
+        this.photoUrl = photoUrl;
+        this.species = species;
+        this.vaccinations = vaccinations;
+        this.weightKg = weightKg;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Breed getBreed() {
+        return breed;
+    }
+
+    public void setBreed(Breed breed) {
+        this.breed = breed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public List<Consultation> getConsultations() {
+        return consultations;
+    }
+
+    public void setConsultations(List<Consultation> consultations) {
+        this.consultations = consultations;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMicrochip() {
+        return microchip;
+    }
+
+    public void setMicrochip(String microchip) {
+        this.microchip = microchip;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public Species getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(Species species) {
+        this.species = species;
+    }
+
+    public List<Vaccination> getVaccinations() {
+        return vaccinations;
+    }
+
+    public void setVaccinations(List<Vaccination> vaccinations) {
+        this.vaccinations = vaccinations;
+    }
+
+    public BigDecimal getWeightKg() {
+        return weightKg;
+    }
+
+    public void setWeightKg(BigDecimal weightKg) {
+        this.weightKg = weightKg;
+    }
 }

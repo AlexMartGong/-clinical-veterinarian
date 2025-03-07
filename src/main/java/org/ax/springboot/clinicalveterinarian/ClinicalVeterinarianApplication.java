9df707c23c35c1afa9cb9adc913cdb89cec63ef0
AlexMartGong <155708055+AlexMartGong@users.noreply.github.com>
2025-03-07 07:46:10 -0600

    initialize Spring Boot project with Maven setup and basic application structure

---
+++ src/main/java/org/ax/springboot/clinicalveterinarian/ClinicalVeterinarianApplication.java
@@ -0,0 +1,13 @@
+package org.ax.springboot.clinicalveterinarian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalVeterinarianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicalVeterinarianApplication.class, args);
+    }
+
+}

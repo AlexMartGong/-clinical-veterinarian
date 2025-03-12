8f6344310eccbd0bf189a530b83f045579551d87
AlexMartGong <155708055+AlexMartGong@users.noreply.github.com>
2025-03-12 14:08:29 -0600

    add JWT authentication filter for user login

---
+++ src/main/java/org/ax/springboot/clinicalveterinarian/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,56 @@
+package org.ax.springboot.clinicalveterinarian.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.ax.springboot.clinicalveterinarian.entities.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.security.Key;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    private static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager1) {
+        this.authenticationManager = authenticationManager1;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+
+        User user = null;
+        String username = null;
+        String password = null;
+
+        try {
+            user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+            username = user.getUsername();
+            password = user.getPassword();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+        return authenticationManager.authenticate(authenticationToken);
+
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+}
